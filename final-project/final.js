1ba6b16b54efa0f492c1438c3c9c1705bab075c2
fralcala <fralcala@iu.edu>
2024-12-13 23:42:16 -0500

    login form completed

--- final-project/final.js
+++ final-project/final.js
@@ -1,9 +1,52 @@
 let username = document.getElementById("username");
+let password = document.getElementById("password");
+
 const users = [];
 
 const section = document.getElementById("section");
 
-function login() {}
+function login() {
+  let containsNumber = false;
+  let containsUppercase = false;
+
+  for (let i = 0; i < password.value.length; i++) {
+    const currentCharacter = password.value[i];
+
+    // Is there at least 1 number
+    if (isNaN(currentCharacter) === false) {
+      containsNumber = true;
+    }
+
+    // Is there at least 1 uppercase letter
+    if (
+      currentCharacter == currentCharacter.toUpperCase() &&
+      isNaN(currentCharacter)
+    ) {
+      containsUppercase = true;
+    }
+  }
+
+  if (username.value === "" || password.value === "") {
+    alert("Username or Password empty");
+  } else if (password.value.length < 8) {
+    alert("Password must be 8 characters or longer");
+    console.log("no");
+  } else if (containsNumber == false && containsUppercase == false) {
+    alert("You need at leat one number and one uppercase letter");
+  } else {
+    section.innerHTML = "";
+    section.innerHTML += `<button onclick="addTask()">Add Task</button>`;
+  }
+}
+
+let tasks = [];
+
+let taskId = 0;
+function showTasks() {
+  section.innerHTML = "";
+
+  myFriends.forEach(function (friend, friendIndex) {});
+}
 
 function addTask() {
   const addNewTask = prompt("Task name:");
@@ -11,10 +54,29 @@ function addTask() {
   newTask.classList.add("task");
   newTask.innerHTML += `
   <h3>${addNewTask}</h3>
-  <button class="red">Remove</button>
-  <button class="blue">Change Text</button>
+  <button class="red" onclick="removeTask(this)">Remove</button>
+  <button class="blue" onclick="renameTask()">Change Text</button>
   `;
   newTask.dataset.text = newTask;
 
+  tasks.push(newTask);
+
   section.appendChild(newTask);
+  console.log(tasks);
 }
+
+function removeTask(button) {
+  // Remove the task from the DOM
+  const taskToRemove = button.parentElement;
+  taskToRemove.remove();
+
+  // Removes the task from the `tasks` array
+  const index = tasks.indexOf(taskToRemove);
+  if (index > -1) {
+    tasks.splice(index, 1);
+  }
+
+  console.log(tasks);
+}
+
+function renameTask() {}
